edafc92cfdd05f2fc6c283ca64050bdfffc9b1de
Alexander Lapik <Alapik101@hotmail.com>
2020-11-25 14:01:49 -0500

    Update math_test.py
    no longer raises a ValueError once a non interger is passed through the input.

--- math_test.py
+++ math_test.py
@@ -7,27 +7,35 @@ Basic arithmetic test
 import random
 
 
-def main(numberOfQuestions=10, studentName=input()):
+def arithmetic_test(numberOfQuestions=10, studentName=input("Name: ")):
     correct_answers = 0  # Keep track of correct answers
 
-    for question_number in range(0, numberOfQuestions):  # Repeat x many times for number of questions
+    # Repeat x many times for number of questions
+    for _ in range(0, numberOfQuestions):
 
         numbers = sorted([random.randint(1, 10) for _ in range(0, 2)],
                          reverse=True)  # Create an array with 2 random numbers for the equation
 
-        operation = random.randint(0, 1)  # Create a random number, 0 == addition and 1 == subtraction
+        # Generate a random number, 0 == addition and 1 == subtraction
+        operation = random.randint(0, 1)
 
-        print(numbers[0], ''.join(['-' if num == '1' else '+' for num in str(operation)]), numbers[1])  # Display question
+        print(numbers[0], ''.join(
+            ['-' if num == '1' else '+' for num in str(operation)]), numbers[1])  # Display question
 
-        if operation == 0:  # Addition
+        try:  # This will prevent the program from rasing an error.
 
-            if int(input()) == numbers[0] + numbers[1]:
-                correct_answers += 1
+            if operation == 0:  # Addition
 
-        else:  # Subtraction
+                if int(input()) == numbers[0] + numbers[1]:
+                    correct_answers += 1
 
-            if int(input()) == numbers[0] - numbers[1]:
-                correct_answers += 1
+            else:  # Subtraction
+
+                if int(input()) == numbers[0] - numbers[1]:
+                    correct_answers += 1
+
+        except ValueError:
+            continue
 
     print(
         ''.join(studentName + ', ' + str(correct_answers) + ' out of ' + str(numberOfQuestions)))  # Print test results
@@ -36,4 +44,4 @@ def main(numberOfQuestions=10, studentName=input()):
 
 
 if __name__ == "__main__":
-    main()
+    arithmetic_test()
